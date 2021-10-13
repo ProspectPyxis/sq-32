d1b0de1ab69a893c4e96f02e2eabf00e7da0eb4f
ProspectPyxis <prospectpyxis@gmail.com>
2021-10-13 10:54:21 +0700

    feat: new sq-32 Error type

--- src/lib.rs
+++ src/lib.rs
@@ -6,5 +6,21 @@ pub mod run;
 pub mod utils;
 pub mod worker;
 
+use std::io;
+use std::num;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("input is empty")]
+    EmptyInputError,
+    #[error("invalid input: {0}")]
+    InvalidInputError(String),
+    #[error("bad data: {0}")]
+    BadDataError(String),
+    #[error("{0:?}")]
+    ParseIntError(#[from] num::ParseIntError),
+}
+
 #[cfg(test)]
 mod tests;
