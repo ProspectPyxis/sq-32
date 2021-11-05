5ccf2ca8911f8f0d52bc20a5397af71d4bda9043
ProspectPyxis <prospectpyxis@gmail.com>
2021-11-05 22:11:51 +0700

    feat: bit module

---
+++ src/bit.rs
@@ -0,0 +1,20 @@
+use crate::error::BitError;
+use std::mem;
+
+pub fn get_first_on_bit_pos<T: Into<u64> + Copy>(b: T) -> Result<usize, BitError> {
+    (0..mem::size_of::<T>())
+        .find(|x| b.into() as usize & (1 << x) != 0)
+        .ok_or(BitError::UnexpectedZero)
+}
+
+pub fn get_all_on_bits<T: Into<u64> + Copy>(b: T) -> Vec<usize> {
+    let mut ons: Vec<usize> = Vec::new();
+
+    for i in 0..mem::size_of::<T>() {
+        if b.into() as usize & (1 << i) != 0 {
+            ons.push(i);
+        }
+    }
+
+    ons
+}
